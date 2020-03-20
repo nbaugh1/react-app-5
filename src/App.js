2c67e0e515421c9bbc56ec1ff2c4a4240cb1377b
nbaugh1 <nbaugh1@gmail.com>
2020-03-19 22:07:05 -0400

    add node-sass and custom.scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <h6>TESTESTESTSETSETEST</h6>
       </div>
     );
   }
