fedf0fbd98fcb99c5bdf1799bbe3d1828d210dd2
nbaugh1 <nbaugh1@gmail.com>
2020-03-19 22:11:09 -0400

    import custom.scss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './custom.scss';
 
 render(<App />, document.getElementById('root'))
 
