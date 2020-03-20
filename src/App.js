7991e43981ad0a3c65558d399d5e8cfba60b352d
nbaugh1 <nbaugh1@gmail.com>
2020-03-19 20:08:19 -0400

    update branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar'
+import TypoGraphy from '@material-ui/core/Typography'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>content goes here</p>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar color="primary" position="static">
+          <Toolbar>
+            <TypoGraphy variant="title"
+              color="inherit"
+            >
+              My header
+           </TypoGraphy>
+          </Toolbar>
+        </AppBar>
+
+      </div>
+    );
+  }
 }
-
 export default App;
